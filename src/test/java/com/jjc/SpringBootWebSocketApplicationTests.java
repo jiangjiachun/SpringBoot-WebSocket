5d32e9b835cc81998271484b6a4424ededf73171
姜佳春 <jjccoke@gmail.com>
2020-06-28 10:37:08 +0800

    springboot websocket

---
+++ src/test/java/com/jjc/SpringBootWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
